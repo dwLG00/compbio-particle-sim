c6eddf3a7e70f9dbf3952c9c78ea76a618fceaca
exlipse1 <125318342+exlipse1@users.noreply.github.com>
2023-03-02 12:35:55 -0800

    Add files via upload

---
+++ poggo.js
@@ -0,0 +1,37 @@
+// Initialize the 2D array
+const arr2D = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 2, 3]
+];
+
+// Initialize an object to store the value counts
+const valueCounts = {};
+
+// Loop through the 2D array and increment the value counts
+for (let i = 0; i < arr2D.length; i++) {
+  const row = arr2D[i];
+  for (let j = 0; j < row.length; j++) {
+    const value = row[j];
+    if (value in valueCounts) {
+      valueCounts[value]++;
+    } else {
+      valueCounts[value] = 1;
+    }
+  }
+}
+const valueCounts2 = Object.values(valueCounts);
+const sum = valueCounts2.reduce((a, b) => a + b, 0);
+
+// Normalize the list
+const normalizedList = valueCounts2.map((value) => value / sum);
+
+// Print the normalized list
+let entropy = 0;
+for (const frequency of normalizedList) {
+  entropy -= frequency * Math.log2(frequency);
+}
+
+// Print the entropy
+console.log(entropy);
